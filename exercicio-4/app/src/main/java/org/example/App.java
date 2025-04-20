512851ac35d769dcd6198c140a4dc37fdd076b9b
Willian Ferreira dos Santos <willsantfer@gmail.com>
2025-04-19 23:20:52 -0300

    Edita App.java

--- exercicio-4/app/src/main/java/org/example/App.java
+++ exercicio-4/app/src/main/java/org/example/App.java
@@ -4,11 +4,9 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    
+    public static void main(String[] args) {
+        
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
 }
